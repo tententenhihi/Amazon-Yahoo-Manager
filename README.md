cbeed7e7c85f4631d9c6eab06da42d598d67edc4
bacnt <bacnt2412@gmail.com>
2021-06-21 10:19:34 +0700

    add detail group asin

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 - Tạo file settings.js từ file setting-default.js trong src/configs
 
 # Update Server
-cd Amazon-Yahoo-Manager/server/; git pull; npm run build; pm2 delete www; pm2 start dist/bin/www.js
-cd Amazon-Yahoo-Manager/client/; git pull; npm run build; sudo rm -r /var/www/dist; sudo cp -r dist /var/www/dist
+cd /root/Amazon-Yahoo-Manager/server/; git pull; npm run build; pm2 delete www; pm2 start dist/bin/www.js
+cd /root/Amazon-Yahoo-Manager/client/; git pull; npm run build; sudo rm -r /var/www/dist; sudo cp -r dist /var/www/dist
--- client/src/views/Asin/components/TableGroupAsin.vue
+++ client/src/views/Asin/components/TableGroupAsin.vue
@@ -19,8 +19,31 @@
         <template v-slot:[`item.created`]="{ item }">
           {{ $moment(item.created).format("DD/MM/YYYY") }}
         </template>
+        <template v-slot:[`item.actions`]="{ item }">
+          <span
+            style="color: blue; cursor: pointer"
+            @click="
+              groupAsinDetail = item;
+              showDialogDetail = true;
+            "
+            >Detail</span
+          >
+        </template>
       </v-data-table>
     </v-card>
+    <v-dialog v-model="showDialogDetail" max-width="500px">
+      <v-card v-if="groupAsinDetail">
+        <v-card-title>{{ groupAsinDetail.groupId }}</v-card-title>
+        <v-card-text>
+          <table>
+            <tr v-for="(item, index) in groupAsinDetail.asins" :key="index">
+              <th class="font-weight-bold">{{ item.code }}:</th>
+              <td>{{ item.status }}: {{ item.statusMessage }}</td>
+            </tr>
+          </table>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -29,6 +52,8 @@ export default {
   data() {
     return {
       search: "",
+      showDialogDetail: false,
+      groupAsinDetail: null,
       headers: [
         {
           text: "NO",
@@ -36,8 +61,12 @@ export default {
         },
         { text: "Group ID", value: "groupId" },
         { text: "Count Asin", value: "countAsin" },
-        { text: "Count Asin Geted Product", value: "countAsinGetProductSuccess" },
-        { text: "Created", value: "created" }
+        {
+          text: "Count Asin Geted Product",
+          value: "countAsinGetProductSuccess"
+        },
+        { text: "Created", value: "created" },
+        { text: "Actions", value: "actions" }
       ]
     };
   },
