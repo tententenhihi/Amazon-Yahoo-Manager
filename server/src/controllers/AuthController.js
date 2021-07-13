127d19d1dd63c9b43abf60f85df239777ce48557
nghdung1996 <huydung17101996@gmail.com>
2021-07-13 15:48:41 +0700

    update login from username

--- server/src/controllers/AuthController.js
+++ server/src/controllers/AuthController.js
@@ -56,6 +56,11 @@ class AuthController {
                 let userLogin = await UserModel.findOne({
                     email,
                 });
+                if (!userLogin) {
+                    userLogin = await UserModel.findOne({
+                        username: email
+                    });
+                }
                 if (userLogin) {
                     if (userLogin.status === 'LOCKED') {
                         return response.error400({ message: 'アカウントがロックされました' });
