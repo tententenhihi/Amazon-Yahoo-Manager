e1215a252421c25e6c8931d8169cfd2abec0a84b
bacnt <bacnt2412@gmail.com>
2021-07-31 15:36:39 +0700

    fix Image Controller

--- server/src/controllers/ImageInsertionController.js
+++ server/src/controllers/ImageInsertionController.js
@@ -1,14 +1,14 @@
 import Response from '../utils/Response';
 import ImageInsertionService from '../services/ImageInsertionService';
-import UploadFile from '../helpers/UploadFile'
-import Path from 'path'
-import fs from 'fs'
+import UploadFile from '../helpers/UploadFile';
+import Path from 'path';
+import fs from 'fs';
 
 export default class ImageInsertionController {
     static async get(req, res) {
         let response = new Response(res);
-        let userId = req.user._id
-        let {yahoo_account_id} = req.query
+        let userId = req.user._id;
+        let { yahoo_account_id } = req.query;
         try {
             let imageInsertion = await ImageInsertionService.get(userId, yahoo_account_id);
             return response.success200({ imageInsertion });
@@ -18,11 +18,11 @@ export default class ImageInsertionController {
         }
     }
 
-    static async update (req, res) {
+    static async update(req, res) {
         let response = new Response(res);
         try {
             let payload = JSON.parse(req.body.payload);
-            let data = { ...payload, user_id: req.user._id }
+            let data = { ...payload, user_id: req.user._id };
             if (req.files && payload.images.length) {
                 for (let index = 0; index < payload.images.length; index++) {
                     const element = req.files[`image-` + index];
@@ -32,28 +32,28 @@ export default class ImageInsertionController {
                 }
             }
 
-            const imageInsertionFolder = 'image-insertion/default/'
+            const imageInsertionFolder = 'image-insertion/default/';
             const pathDestFolder = Path.join(__dirname, `../../uploads/${imageInsertionFolder}`);
             let defaultImages = [];
 
             for (let index = 0; index < data.images.length; index++) {
-                if(!data.images[index]) {
+                if (!data.images[index]) {
                     if (!defaultImages.length) {
-                        fs.readdirSync(pathDestFolder).forEach(file => {
-                            defaultImages.push(imageInsertionFolder + file)
+                        fs.readdirSync(pathDestFolder).forEach((file) => {
+                            defaultImages.push(imageInsertionFolder + file);
                         });
                     }
-                    data.images[index] = defaultImages[index]
+                    data.images[index] = defaultImages[index];
                 }
             }
 
             let result = await ImageInsertionService.update(data);
             if (result) {
-                response.success200(result);
+                return response.success200(result);
             }
-            response.success200({});
+            return response.success200({});
         } catch (error) {
-            response.error500(error)
+            response.error500(error);
         }
     }
 }
