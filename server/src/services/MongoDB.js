90cd8c1dfe91e2b79e52c4eda7f98233f1f6e205
bacnt2412 <bacnt2412@gmail.com>
2021-09-30 23:15:42 +0700

    fix

--- server/src/services/MongoDB.js
+++ server/src/services/MongoDB.js
@@ -4,34 +4,36 @@ import Fs from 'fs';
 
 class MongoDB {
     static connect(callback) {
-        // var mongoOptions = {
-        //     keepAlive: 1,
-        //     connectTimeoutMS: 30000,
-        //     // reconnectTries: 30,
-        //     // reconnectInterval: 2000,
-        //     useNewUrlParser: true,
-        //     useFindAndModify: false,
-        //     useUnifiedTopology: true,
-        //     // user: MONGODB_USER,
-        //     // pass: MONGODB_PASSWORD,
-        //     // authSource:  'admin'
-        // };
-        // let MONGODB_CONNECTION_STRING = config.get('MONGODB_HOST') + config.get('MONGODB_DATABASE_NAME');
+        let mongoOptions = null;
+        if (config.get('env') === 'development') {
+            mongoOptions = {
+                keepAlive: 1,
+                connectTimeoutMS: 30000,
+                // reconnectTries: 30,
+                // reconnectInterval: 2000,
+                useNewUrlParser: true,
+                useFindAndModify: false,
+                useUnifiedTopology: true,
+                // user: MONGODB_USER,
+                // pass: MONGODB_PASSWORD,
+                // authSource:  'admin'
+            };
+        } else {
+            mongoOptions = {
+                keepAlive: 1,
+                connectTimeoutMS: 30000,
+                useNewUrlParser: true,
+                useFindAndModify: false,
+                useUnifiedTopology: true,
+                user: config.get('MONGODB_USERNAME'),
+                pass: config.get('MONGODB_PASSWORD'),
+                ssl: true,
+                sslValidate: false,
+                sslCA: 'mongodb.pem',
+            };
+        }
 
-        var mongoOptions = {
-            keepAlive: 1,
-            connectTimeoutMS: 30000,
-            useNewUrlParser: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true,
-            user: 'usermaster',
-            pass: '123456789',
-            ssl: true,
-            sslValidate: false,
-            sslCA: 'mongodb.pem',
-        };
-        let MONGODB_CONNECTION_STRING = 'mongodb://yahoo-auctions-mongo-db.cluster-cw3mf6t08xn4.ap-northeast-1.docdb.amazonaws.com:27017/Amazon-yahoo-manager';
-
+        let MONGODB_CONNECTION_STRING = config.get('MONGODB_HOST') + config.get('MONGODB_DATABASE_NAME');
         // ====================
         mongoose.connect(MONGODB_CONNECTION_STRING, mongoOptions);
         mongoose.Promise = global.Promise;
